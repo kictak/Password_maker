f8c5f0ccc39eea6c40cff38f98571ce4b2bdad21
Br <183182231+kictak@users.noreply.github.com>
2026-04-07 18:05:19 +0700

    75%

--- app.py
+++ app.py
@@ -2,53 +2,75 @@ import tkinter as tk
 from tkinter import ANCHOR, CENTER, Button, ttk
 import time
 from types import CellType
+import password_play as pp
 
-
+## Присвоение root - Tk + навзание + размер окна приложения + запрет на изменение окна
 root = tk.Tk()
 root.title("RedPass")
 root.geometry("400x200+760+440")
 root.resizable(False, False)
-
-# кнопка запроса генерации пароля
-open_button = ttk.Button(text="Сгенерировать пароль")
-open_button.place(x=120, y=130, width=155, height=35)
-
-# вывод сегенрированного пароля
+size = 8
+good_password = "Тут чего-то не хватает...??"
 
 
 ## Выбор длинны пароля
-def change(new_val):
-    pass_size["text"] = int(scale.get())
+def change(new_value):
+    size = int(float(new_value))
+    pass_size["text"] = size
+    print(size)
 
 
+## Отображение выбраного размера пароля
 pass_size = ttk.Label()
 pass_size.pack(anchor="nw")
 pass_size.place(x=270, y=100, width=19, height=20)
-##
 
-## Настройка стиля для Scale
+## Настройка стиля виджетов
 style = ttk.Style()
 style.theme_use("clam")
+# настрока стимле ползунка - scale
 style.configure(
-    "TScale",
+    "TScale",  #  отвечает за стиль всех ползунков для изменеиния
+    #  конреткного виджета мне надо писать "My.TScale"
+    #  и указывать в парметрах style="My.TScale"
     background="white",
     troughcolor="gray",
     sliderlength=10,
 )
-##
+# настройка стиля текстового окна
+style.configure("My.Textstyle", background="gray")
+
+
 ## Текстовая вставка выберите размер пароля
 info_pass_size = tk.Label(text="Выберите длину пароля")
 info_pass_size.place(x=100, y=80, width=210, height=20)
-##
+
 ## Ползунок выбора размера пароля
 scale = ttk.Scale(orient="horizontal", length=200, from_=8, to=50, command=change)
 scale.pack(anchor="nw")
 scale.place(x=120, y=100, width=150, height=20)
-##
+
+
+# pp.generate_password(int(scale.get()))
+ready_password = tk.Label(text=good_password)
+ready_password.place(x=0, y=20, width=400, height=20)
+ready_password.configure(background="white")
+
+
+## Вывод пароля
+def gen_pass():
+    size = int(scale.get())
+    ready_password["text"] = pp.generate_password(size)
+    print(good_password)
+
 
 ## Иконка приложения
 root.iconbitmap(r"./icon.ico")
-##
+
+
+## Кнопка запроса генерации пароля
+open_button = ttk.Button(text="Сгенерировать пароль", command=gen_pass)
+open_button.place(x=120, y=130, width=155, height=35)
+
 
 root.mainloop()
-print()
--- password_play.py
+++ password_play.py
@@ -2,8 +2,6 @@ import secrets
 import string
 import requests
 import hashlib
-import time
-import os
 
 
 def generate_password(pass_size):
@@ -12,47 +10,29 @@ def generate_password(pass_size):
     while True:
         password = "".join(secrets.choice(characters) for _ in range(pass_size))
         if (
-            any(c.islower() for c in password) >= pass_size % 4
-            and any(c.isupper() for c in password) >= pass_size % 4
-            and sum(c.isdigit() for c in password) >= pass_size % 4
-            and (check_password_leak(password) == 0)
+            any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and sum(c.isdigit() for c in password) >= 2
+            and sum(c in safe_symbols for c in password) >= 2
         ):
-            os.system("cls")
-            print(
-                f"""
-            \033[32m
-                    ПАРОЛЬ СОЗДАН И ПРОВЕРЕН\n
-                         ВСЁ ГОТОВО\n
-                {password}\n
-            \033[0m
-            """
-            )
-            return password
+            if check_password_leak(password) == 0:
+                return password
 
 
 def check_password_leak(password):
     # 1. Создаем SHA-1 хеш пароля и переводим в верхний регистр
     sha1_password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
-    print(sha1_password)
-    time.sleep(0)
     # 2. Берем первые 5 символов (префикс) и остальную часть (суффикс)
     prefix = sha1_password[:5]
     suffix = sha1_password[5:]
-
     # 3. Делаем запрос к API
     url = f"https://api.pwnedpasswords.com/range/{prefix}"
     response = requests.get(url)
-
+    print(response)
     if response.status_code != 200:
         return "Ошибка при запросе к API"
-
     # 4. Ищем наш суффикс в ответе (ответ — список строк вида "СУФФИКС:КОЛИЧЕСТВО")
-    hashes = (line.split(":") for line in response.text.splitlines())
-    for h, count in hashes:
+    for h, count in (line.split(":") for line in response.text.splitlines()):
         if h == suffix:
             return 1
-
     return 0
-
-
-password = generate_password(50)
